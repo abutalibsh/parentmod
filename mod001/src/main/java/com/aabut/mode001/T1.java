3528732456379875edc89047c6349068a321ec79
Ahmad Abutalib <abutalibsh@gmail.com>
2021-08-01 08:21:02 -0900

    Initial commit

---
+++ mod001/src/main/java/com/aabut/mode001/T1.java
@@ -0,0 +1,12 @@
+package com.aabut.mode001;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class T1 {
+	
+	private String name;
+
+}
---
+++ mod001/src/main/java/com/aabut/mode001/Task.java
@@ -0,0 +1,9 @@
+package com.aabut.mode001;
+
+public class Task {
+
+	public static void main(String[] args) {
+		System.out.println("Running mod001..." + new T1("Ahmad"));
+	}
+
+}
